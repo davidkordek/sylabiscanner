4d6b47638b39ab493f6d59e5dbe6115be94d3ea8
davidkordek <34850947+davidkordek@users.noreply.github.com>
2021-05-03 21:17:24 -0500

    Update PDFServlet.java

--- PDFServlet.java
+++ PDFServlet.java
@@ -1,12 +1,10 @@
 package com.example.demo3;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 import org.json.JSONObject;
@@ -18,10 +16,8 @@ import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "PDFServlet", value = "/PDFServlet")
 public class PDFServlet extends HttpServlet {
@@ -52,14 +48,14 @@ public class PDFServlet extends HttpServlet {
         }
         PDDocument document = new PDDocument();
 
-        PDPage page = new PDPage();
+        PDPage page = new PDPage(PDRectangle.A4);
         document.addPage(page);
         PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
         contentStream.beginText();
-        contentStream.newLineAtOffset(25, 700);
+        contentStream.newLineAtOffset(25, 800);
         contentStream.setLeading(14.5f);
-        contentStream.setFont( PDType1Font.TIMES_ROMAN	, 12 );
+        contentStream.setFont( PDType1Font.TIMES_ROMAN	, 11 );
 
         for ( JSONObject obj: cal.getCalendar()) {
             ArrayList<String> text;
@@ -68,19 +64,20 @@ public class PDFServlet extends HttpServlet {
             sb.append("Date: "+(String) obj.get("eventDate")+ " ");
             //sb.append((String) obj.get("eventType") +" ");
             sb.append("Description: "+(String) obj.get("eventDescription")+ " ");
+
+            //this replaces letters in the string to make it look nicer and we remove new line characters
+            //because the program will crash if we dont.
             String t = sb.toString().replace("\n", " ").replace("\r", " ").replace(","," ").replace("-","");
             t = t.replaceAll("\\(\\d\\d/\\d\\d\\)"," ");
-            //text will stored into smaller arrays of specified size
-            //t can be modified to your liking, i got rid of redundent symbols and dates
-            text = splitEqually(t,110);
-            //you can look text adding it to content stream. This next line will print to stream
-            contentStream.showText(text.get(0));
-            //after adding to stream we should add a new line.
-            contentStream.newLine();
-            //I can add code here to stop stream once the length is past the pdf limit.
-            //I would close stream and reopen the stream with parameters similar to like 59-62.
-        }
 
+
+            //this line of code removes duplicate words in the string.
+            String a = Arrays.stream( t.split("\\s+")).distinct().collect(Collectors.joining(" ") );
+            contentStream.showText(a);
+            contentStream.newLine();
+
+        }
+        //the syllabus will be short enough that one page will be enough.
         contentStream.endText();
         contentStream.close();
         //document.addPage(page);
@@ -89,14 +86,5 @@ public class PDFServlet extends HttpServlet {
         System.out.println("PDF created");
 
     }
-    public static ArrayList<String> splitEqually(String text, int size) {
-        // Give the list the right capacity to start with. You could use an array
-        // instead if you wanted.
-        ArrayList<String> t = new ArrayList<String>((text.length() + size - 1) / size);
 
-        for (int start = 0; start < text.length(); start += size) {
-            t.add(text.substring(start, Math.min(text.length(), start + size)));
-        }
-        return t;
-    }
 }
